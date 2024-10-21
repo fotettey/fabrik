ab27682461505f6fd3df4ca51f05a5fcdf113bcf
Festus Ofori-Tettey <fotettey@Festuss-MBP.fritz.box>
2024-10-21 02:03:17 +0200

    apps/ data dir config postgres db

--- apps/postgres/init-db.sh
+++ apps/postgres/init-db.sh
@@ -4,7 +4,7 @@ set -e
 ## https://hub.docker.com/_/postgres
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER sonarqube;
+	CREATE USER sonarqube WITH PASSWORD 'sonarqube';
 	CREATE DATABASE sonarqube;
 	GRANT ALL PRIVILEGES ON DATABASE sonarqube TO sonarqube;
 EOSQL

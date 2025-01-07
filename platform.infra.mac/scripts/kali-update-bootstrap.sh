b0198a7c2cebc2e4edb617617bc82796a68222e8
fotettey <fotettey@gmail.com>
2025-01-07 16:40:32 +0100

    updated nmap script

--- platform.infra.mac/scripts/kali-update-bootstrap.sh
+++ platform.infra.mac/scripts/kali-update-bootstrap.sh
@@ -14,7 +14,7 @@ sudo apt-get clean
 # proxy_dns - uncommented, and lastly that proxy IPs in last section are configured based on video guide: https://youtu.be/dJjQoxwyNCc?t=730
 
 ## Copying proxychains config:
-sudo cp /vagrant/projects/codebank/scripts/proxychains4.conf /etc/
+sudo cp /vagrant/projects/codebank/scripts/proxychains4.conf /etc/ || sudo cp ~/projects/fabrik/platform.infra.mac/scripts/proxychains4.conf /etc/
 sudo chown $whoami:$whoami /etc/proxychains4.conf
 
 # 2. Install TOR service/browser on Kali instance

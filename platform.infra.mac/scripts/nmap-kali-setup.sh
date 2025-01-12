0b808d85755b801bfdddb51da4f9ca6e1eb724ad
fotettey <fotettey@gmail.com>
2025-01-12 08:39:23 +0100

    added py version format logic to nmap script

--- platform.infra.mac/scripts/nmap-kali-setup.sh
+++ platform.infra.mac/scripts/nmap-kali-setup.sh
@@ -15,8 +15,11 @@ cd ~/nmap
 ## for Errors such as "...make: *** [Makefile:364: install-zenmap] Error 1"
 ## It is required to downgrade pip:setuptools to 62.0.0 as discussed here: https://github.com/nmap/nmap/issues/2714#issuecomment-1871329317
 ## REQUIRED FIX: https://askubuntu.com/questions/1471898/makefile372-install-zenmap-error-1
-cd /usr/lib/python3.11
-sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED.original
+
+PYTHON_VERSION=$(python3 --version | awk '{print $2}' | cut -d. -f1,2)
+
+cd /usr/lib/python$PYTHON_VERSION
+sudo rm /usr/lib/$PYTHON_VERSION/EXTERNALLY-MANAGED.original
 sudo pip install setuptools==62.0.0
 pip show setuptools
 cd ~/nmap

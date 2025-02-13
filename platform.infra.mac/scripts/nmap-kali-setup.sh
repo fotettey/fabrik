bb6b8e282196fa1904613a13cb225f60c71afc0a
Festus O.T <82847114+fotettey@users.noreply.github.com>
2025-02-13 01:12:58 +0100

    Scripts update (#6)
    * updated nmap script
    
    * added py version format logic to nmap script
    
    * formatted nmap script for py version logic and py env install options
    
    * updated nmap script for py version logic and py env install options

--- platform.infra.mac/scripts/nmap-kali-setup.sh
+++ platform.infra.mac/scripts/nmap-kali-setup.sh
@@ -9,18 +9,52 @@ sudo apt install libssl-dev autoconf make g++ subversion -y
 cd ~
 svn co https://svn.nmap.org/nmap/
 cd ~/nmap
-./configure --help
-./configure
-# make
+
 ## for Errors such as "...make: *** [Makefile:364: install-zenmap] Error 1"
 ## It is required to downgrade pip:setuptools to 62.0.0 as discussed here: https://github.com/nmap/nmap/issues/2714#issuecomment-1871329317
 ## REQUIRED FIX: https://askubuntu.com/questions/1471898/makefile372-install-zenmap-error-1
-cd /usr/lib/python3.11
-sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED.original
-sudo pip install setuptools==62.0.0
-pip show setuptools
-cd ~/nmap
-sudo make install
+
+PREFERRED_PYTHON_VERSION=3.11
+PYTHON_VERSION=$(python3 --version | awk '{print $2}' | cut -d. -f1,2)
+
+# Provisioning Other VMs
+PREFERRED_PYTHON_VERSION=3.11
+PYTHON_VERSION=$(python3 --version | awk '{print $2}' | cut -d. -f1,2)
+
+if [ "$PYTHON_VERSION" = "$PREFERRED_PYTHON_VERSION" ]; then
+    echo "Python3.11 installed and present"
+    ## Configuring & Installing NMAP
+     #sudo apt update
+    sudo apt install -y python3-venv  # Ensure `venv` is installed
+    sudo apt install -y libcairo2 libcairo2-dev libgirepository1.0-dev build-essential pkg-config python3-dev gir1.2-gtk-3.0
+    cd /usr/lib/python$PREFERRED_PYTHON_VERSION
+    sudo rm /usr/lib/$PREFERRED_PYTHON_VERSION/EXTERNALLY-MANAGED.original
+    sudo pip install setuptools==62.0.0
+    sudo pip install build
+    pip show setuptools
+    cd ~/nmap
+    ./configure --help
+    ./configure
+    sudo make
+    sudo make install
+elif [ "$PYTHON_VERSION" != "$PREFERRED_PYTHON_VERSION" ]; then
+    echo "Different Python3 version installed and present"
+    ## Configuring & Installing NMAP
+    sudo apt update
+    sudo apt install -y python3-venv  # Ensure `venv` is installed
+    sudo apt install -y libcairo2 libcairo2-dev libgirepository1.0-dev build-essential pkg-config python3-dev gir1.2-gtk-3.0
+    python3 -m venv myenv          # Create a virtual environment
+    source myenv/bin/activate      # Activate the environment
+    pip install setuptools==62.0.0    # Install packages within the environment
+    sudo pip install build
+    cd ~/nmap
+    ./configure --help
+    ./configure
+    sudo make
+    sudo make install
+else
+    echo "Python not installed, check and fix!"
+fi
 
 ## Zenmap
 ### Run Zenmap:
